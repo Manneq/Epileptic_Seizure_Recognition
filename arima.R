335de52eaf7678fb1fe133ec6b43a7bb0a9fae2b
Manneq <49914533+manneq@users.noreply.github.com>
2020-06-21 17:15:18 +0300

    I'm done

--- arima.R
+++ arima.R
@@ -5,7 +5,7 @@ data <- read.csv("C:/Users/Admin/PycharmProjects/Epileptic_Seizure_Recognition/t
 data <- ts(data, start=0, frequency = 178)
 data <- rollmedian(data, 5)
 
-modelArima = Arima(data, order = c(30,2,100), method = "CSS")
+modelArima = Arima(data, order = c(50,1,80), method = "CSS")
 summary(modelArima)
 forecastResults <- forecast(modelArima, h = 25)$mean
 plot(data, 
@@ -17,47 +17,49 @@ plot(data,
 lines(forecastResults, col="red")
 legend("topright", c("Observations", "Forecast"),  fill = c("blue", "red"))
 
-trainingSet1 <- head(data, 61)
-trainingSet2 <- head(data, 116)
-validationSet1 <- tail(data, 113)
-validationSet1 <- head(validationSet1, 61)
-validationSet2 <- tail(data, 58)
-
-modelArima = Arima(head(data, 61), order = c(25,2,30), method = "CSS")
-summary(modelArima)
-
-forecastResults <- forecast(modelArima, h = 61)$mean
-nRMSE <- sqrt(mean(sum((validationSet1 - forecastResults) ^ 2))) / 
-  mean(validationSet1)
-nRMSE
-
-plot(trainingSet1, 
-     main = "Brain activity of patient 791",
-     xlab = "Time, s",
-     ylab = "Brain activity",
-     col = "blue",
-     xlim = c(0, 1.15))
-lines(validationSet1, col="green")
-lines(forecastResults, col="red")
-legend("topright", c("Training set", "Validation set", "Forecast"),  
-       fill = c("blue", "green", "red"))
+trainingSet2 <- head(data, 140)
+validationSet2 <- tail(data, 34)
 
 modelArima = Arima(head(data, 116), order = c(30,2,80), method = "CSS")
 summary(modelArima)
 forecastResults <- forecast(modelArima, h = 58)$mean
 
-nRMSE <- sqrt(mean(sum((validationSet2 - forecastResults) ^ 2))) / 
+nrmse <- sqrt(mean((tail(data, 58) - forecastResults) ^ 2)) / 
   mean(validationSet1)
-nRMSE
-
-plot(trainingSet2, 
-     main = "Brain activity of patient 791",
+nrmse
+mae <- mean(abs(tail(data, 58) - forecastResults))
+mae
+mse <- mean((tail(data, 58) - forecastResults) ^ 2)
+mse
+plot(data, 
+     main = "Brain activity of patient 791 (nRMSE = 17.03, MAE = 70.33, MSE = 7058.96)",
      xlab = "Time, s",
      ylab = "Brain activity",
      col = "blue",
      xlim = c(0, 1.15))
-lines(validationSet2, col="green")
 lines(forecastResults, col="red")
-legend("topright", c("Training set", "Validation set", "Forecast"),  
-       fill = c("blue", "green", "red"))
+legend("topright", c("Observations", "Forecast"),  
+       fill = c("blue", "red"))
+
+
+modelArima = Arima(head(data, 140), order = c(50,1,80), method = "CSS")
+summary(modelArima)
+forecastResults <- forecast(modelArima, h = 34)$mean
+
+nrmse <- sqrt(mean((tail(data, 34) - forecastResults) ^ 2)) / 
+  mean(validationSet1)
+nrmse
+mae <- mean(abs(tail(data, 34) - forecastResults))
+mae
+mse <- mean((tail(data, 34) - forecastResults) ^ 2)
+mse
+plot(data, 
+     main = "Brain activity of patient 791 (nRMSE = 8.30, MAE = 31.99, MSE = 1678.49)",
+     xlab = "Time, s",
+     ylab = "Brain activity",
+     col = "blue",
+     xlim = c(0, 1.15))
+lines(forecastResults, col="red")
+legend("topright", c("Observations", "Forecast"),  
+       fill = c("blue", "red"))
 
